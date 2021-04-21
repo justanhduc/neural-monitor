1c0ad775974b67a3df2474e0f10360b7b57adace
Duc <adnguyen@yonsei.ac.kr>
2021-04-21 12:07:01 +0900

    more docstrings

--- neural_monitor/monitor.py
+++ neural_monitor/monitor.py
@@ -221,14 +221,32 @@ class Monitor:
 
         from neural_monitor import monitor as mon
 
-        mon.initialize(model_name='foo-model', print_freq=100)
+        # Tensorboard is turned on by default
+        mon.initialize(model_name='foo-model', print_freq=100, use_tensorboard=True)
+        ...
+
+        def calculate_loss(pred, gt):
+            ...
+            training_loss = ...
+            mon.plot('training loss', loss, smooth=.99, filter_outliers=True)
+
+        def calculate_acc(pred, gt):
+            accuracy = ...
+            mon.plot('training acc', accuracy, smooth=.99, filter_outliers=True)
 
         ...
         for epoch in mon.iter_epoch(range(n_epochs)):
             for data in mon.iter_batch(data_loader):
-                loss = net(data)
-                mon.plot('training loss', loss, smooth=.99, filter_outliers=True)
+                pred = net(data)
+                calculate_loss(pred, gt)
+                calculate_acc(pred, gt)
                 mon.imwrite('input images', data['images'], latest_only=True)
+
+            mon.dump('checkpoint.pt', {
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                ...
+            }, method='torch', keep=5)  # keep only 5 latest checkpoints
         ...
 
     Attributes
