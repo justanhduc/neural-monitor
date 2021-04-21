1e4f7b176683b48f10f9d8eb49ace45264a376f8
Duc <adnguyen@yonsei.ac.kr>
2021-04-21 12:23:17 +0900

    fixed doc theme bug

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,6 +34,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.intersphinx",
     'sphinx.ext.viewcode'
+    'cloud'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
